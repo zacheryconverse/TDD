f6c3094b4889b5deb0625118cb30aa95cd86395e
Zachery Converse <zacheryconverse@gmail.com>
2020-10-08 17:06:09 -0600

    test the tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -11,3 +11,8 @@ it('renders correctly', () =>{
 it('initializes the `state` with an empty list of gifts', () => {
   expect(app.state().gifts).toEqual([]);
 });
+
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
+  app.find('.btn-add').simulate('click');
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
+})
